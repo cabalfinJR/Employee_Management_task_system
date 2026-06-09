96be20fd9c29120f004510ee9db203f8db5cbf56
Jan ray <72631289+cabalfinJR@users.noreply.github.com>
2026-06-09 12:57:51 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBicp00YhwfTZGiIPIXaD8Fzgs4vFHc4N8",
+  apiKey: "AIzaSyBicp0QYhwfTZGiIPIXaD8Fzgs4vFHc4N8",
   authDomain: "employee-task-system-web.firebaseapp.com",
   projectId: "employee-task-system-web",
   storageBucket: "employee-task-system-web.firebasestorage.app",
