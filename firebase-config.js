7525a8e38ab4a4c7c97d59b65e85f97345b7fab0
Jan ray <72631289+cabalfinJR@users.noreply.github.com>
2026-06-09 11:26:43 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBicp00YhwfTZGiIPIXaD8Fzgs4vFHc4N8",
+  authDomain: "employee-task-system-web.firebaseapp.com",
+  projectId: "employee-task-system-web",
+  storageBucket: "employee-task-system-web.firebasestorage.app",
+  messagingSenderId: "206113180368",
+  appId: "1:206113180368:web:5dbe3328fd6756f41eacd5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
